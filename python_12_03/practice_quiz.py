eadfaa374ada140bf594469895dcf67cc44ba4ae
appdevs-lee <js100810081@gmail.com>
2024-12-03 16:47:59 +0900

    Set, Dictionary에 대한 연습문제

---
+++ python_12_03/practice_quiz.py
@@ -0,0 +1,27 @@
+# student_score = {'홍의': 97, '원희': 60, '동해': 77, '변수': 79, '창현': 89,}
+
+# sum = 0
+# for score in student_score.values():
+#     sum += score
+
+# print(f"총합 : {sum}")
+# print(f"평균 점수 : {sum / 5}")
+# # sum(student_score.values())
+
+# # for name, score in student_score.items():
+
+# def f1(x):
+#     return student_score[x]    
+
+# print(max(student_score, key=student_score.get))
+# print(max(student_score.keys(), key=f1))
+# print(max(student_score.values()))
+# print(min(student_score.keys(), key=(lambda k: student_score[k])))
+# print(min(student_score.values()))
+
+# like = set(['볶음밥', '라면', '국수', '파스타', '치킨', '짜장면', '국밥'])
+# dislike = set(['국밥', '짬뽕', '찜닭', '파스타', '국수', '카레', '덮밥'])
+# print(like - dislike)
+
+# like = ['볶음밥', '라면', '국수', '파스타', '치킨', '짜장면', '국밥']
+# dislike = ['국밥', '짬뽕', '찜닭', '파스타', '국수', '카레', '덮밥']
