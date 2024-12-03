1001e159475309fa700ee4bcb95d88e20a84b637
appdevs-lee <js100810081@gmail.com>
2024-12-03 15:16:30 +0900

    Dict

---
+++ python_12_03/dict.py
@@ -0,0 +1,30 @@
+# d = {'one' : '하나', 'one' : '하나2', 'one' : '하나3', 'two' : '둘', 'three' : '셋'}
+# print(d)
+
+# d = {'one' : '하나', 'one1' : '하나1', 'one2' : '하나2', 'two' : '둘', 'three' : '셋'}
+# print(d)
+
+# d = {} #dict
+# print(type(d))
+
+# d = {10} #set
+# print(type(d))
+
+# d = dict()
+# print(type(d))
+
+# d = {'one' : 1}
+# print(type(d))
+
+
+# dict([('one' , '하나'), ('two', '둘')]) #대괄호를 이용하여 키와 value를 구별해야 합니다.
+
+# dict(name='leehojun', age=10)
+
+# a = [[10,20], [20,30],[30,40]]
+# print(dict(zip('ABC', a)))
+
+# def returnDict(a, b):
+#     return dict.fromkeys(a, b)
+
+# print(returnDict(('name', 'age', 'grade'), ('이주성', '27', '수')))
